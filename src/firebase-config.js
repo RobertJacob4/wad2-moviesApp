8b7e1f02d097c9648d25d745aa5146cc850807a8
Robert Jacob <Robertjacobios@gmail.com>
2021-11-18 21:54:48 +0000

    Added Firebase to project and created firebase config file with credentials

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAnRTSNG9CNDnfl3DiKfLouvZ2kJIumI8Q",
+    authDomain: "movies-app-authentication.firebaseapp.com",
+    projectId: "movies-app-authentication",
+    storageBucket: "movies-app-authentication.appspot.com",
+    messagingSenderId: "85245196981",
+    appId: "1:85245196981:web:c358b10b7c1edf9bfe5cfc"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app)
+
